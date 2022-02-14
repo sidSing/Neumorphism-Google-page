b230b479d4caf6c1844749b1cf6f3fb6c69b4ee3
sidSing <siddhartsingh@hotmail.com>
2022-02-14 19:47:26 +0530

    Added media Queries and minor changes

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 `use strict`;
 
-// alert(
-//   `'Images' and 'I'm Feeling Lucky' is still underprogress please dont check those out. `
-// );
+alert(
+  `'Images' and 'I'm Feeling Lucky' is still underprogress please dont check those out. `
+);
 
-document.querySelector(".input");
+const searchButton = document.querySelector(".search");
+searchButton.addEventListener("click", function () {
+  const search = prompt(`What are you looking for ?`);
+  search.replaceAll("", "-");
+
+  searchButton.href = `https://www.google.com/search?q${search}`;
+});
